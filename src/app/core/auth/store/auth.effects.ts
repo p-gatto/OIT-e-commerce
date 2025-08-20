32071862b4c2f955ed9519842c88148543db9aa0
p-gatto <p.gatto@overitech.it>
2025-08-20 10:57:45 +0200

    sotiutito tap con map operator, risolto problema di deadlock

--- src/app/core/auth/store/auth.effects.ts
+++ src/app/core/auth/store/auth.effects.ts
@@ -8,94 +8,95 @@ import { Actions, createEffect, ofType, rootEffectsInit } from '@ngrx/effects';
 
 import { AuthActions } from './auth.actions';
 
-export const syncWithLocalStorage = createEffect(
-    (
-        actions$ = inject(Actions),
-    ) => {
-        return actions$.pipe(
-            ofType(rootEffectsInit),
-            tap(() => {
-                const token = localStorage.getItem('token');
-                const displayName = localStorage.getItem('displayName');
-                return AuthActions.initialize({ token, displayName });
-            }),
-            catchError((err) => {
-                console.log(err)
-                return of(err)
-            })
-        );
-    },
-    { functional: true }
-);
-
-export const login = createEffect((
+export const syncWithLocalStorage = createEffect((
     actions$ = inject(Actions),
-    http = inject(HttpClient)
 ) => {
     return actions$.pipe(
-        // Handle `login` action
-        ofType(AuthActions.login),
-        // subscribe the inner (http) observable
-        mergeMap((action) => {
-            // create the parameters to send with the request
-            // username and password are acquired by the action payload
-            const params = new HttpParams()
-                .set('username', action.username)
-                .set('password', action.password)
-
-            // make the http request passing the parameters "{ username: ..., password: ...}"
-            return http.get<{ token: string }>('http://localhost:3001/login', { params })
-                .pipe(
-                    tap((res) => {
-                        localStorage.setItem('token', res.token);
-                    }),
-                    map((res) =>
-                        // dispatch the "loginSuccess" action passing the "token" as payload
-                        AuthActions.loginSuccess({ token: res.token })
-                    ),
-                    catchError(() =>
-                        // dispatch the "loginFail" action
-                        of(AuthActions.loginFail())
-                    )
-                )
+        ofType(rootEffectsInit),
+        map(() => {
+            const token = localStorage.getItem('token')
+            const displayName = localStorage.getItem('displayName')
+            return AuthActions.initialize({ token, displayName });
+        }),
+        catchError((err) => {
+            console.log(err);
+            return of(AuthActions.initialize({ token: null, displayName: null }));
         })
     );
 },
     { functional: true }
 );
 
+export const login = createEffect(
+    (
+        actions$ = inject(Actions),
+        http = inject(HttpClient)
+    ) => {
+        return actions$.pipe(
+            // Handle `login` action
+            ofType(AuthActions.login),
+            // subscribe the inner (http) observable
+            mergeMap((action) => {
+                // create the parameters to send with the request
+                // username and password are acquired by the action payload
+                const params = new HttpParams()
+                    .set('username', action.username)
+                    .set('password', action.password)
 
-export const loginSuccess = createEffect((
-    actions$ = inject(Actions),
-    http = inject(HttpClient)
-) => {
-    return actions$.pipe(
-        // wait the loginSuccess action
-        ofType(AuthActions.loginSuccess),
-        mergeMap((action) =>
-            // get the profile passing the token, that is retrieved by the action payload
-            http.get<{ displayName: string }>('http://localhost:3001/profile', {
-                headers: {
-                    Authorization: `Bearer ${action.token}`
-                }
-            })
-                .pipe(
-                    tap((res) => {
-                        localStorage.setItem('displayName', res.displayName)
-                    }),
-                    // dispatch the getProfileSuccess action is success,
-                    // passing the displayName as payload
-                    map((res) =>
-                        AuthActions.getProfileSuccess({ displayName: res.displayName })
-                    ),
-                    // dispatch the getProfileFail in case of errors
-                    catchError(() =>
-                        of(AuthActions.getProfileFail())
+                // make the http request passing the parameters "{ username: ..., password: ...}"
+                return http.get<{ token: string }>('http://localhost:3001/login', { params })
+                    .pipe(
+                        tap((res) => {
+                            localStorage.setItem('token', res.token);
+                        }),
+                        map((res) =>
+                            // dispatch the "loginSuccess" action passing the "token" as payload
+                            AuthActions.loginSuccess({ token: res.token })
+                        ),
+                        catchError(() =>
+                            // dispatch the "loginFail" action
+                            of(AuthActions.loginFail())
+                        )
                     )
-                )
-        )
-    );
-},
+            })
+        );
+    },
+    { functional: true }
+);
+
+
+export const loginSuccess = createEffect(
+    (
+        actions$ = inject(Actions),
+        http = inject(HttpClient)
+    ) => {
+        return actions$.pipe(
+            // wait the loginSuccess action
+            ofType(AuthActions.loginSuccess),
+            mergeMap((action) =>
+                // get the profile passing the token, that is retrieved by the action payload
+                http.get<{ displayName: string }>('http://localhost:3001/profile', {
+                    headers: {
+                        Authorization: `Bearer ${action.token}`
+                    }
+                })
+                    .pipe(
+                        tap((res) => {
+                            localStorage.setItem('displayName', res.displayName)
+                        }),
+                        // dispatch the getProfileSuccess action is success,
+                        // passing the displayName as payload
+                        map((res) =>
+                            AuthActions.getProfileSuccess({ displayName: res.displayName })
+                        ),
+                        // dispatch the getProfileFail in case of errors
+                        catchError(() =>
+                            of(AuthActions.getProfileFail())
+                        )
+                    )
+            )
+        );
+    },
     { functional: true }
 );
 
